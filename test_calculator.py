059924a900c71aea35a9280c65e98992dc0573a6
ranadheer314 <ranadheer314@users.noreply.github.com>
2022-09-10 16:27:16 -0400

    add test for multiplication

--- test_calculator.py
+++ test_calculator.py
@@ -8,3 +8,6 @@ class TestCalculator:
 
     def test_subtraction(Self):
         assert 2 == calculator.subtract(4, 2)
+
+    def test_multipication(self):
+        assert 100 == calculator.multiply(10,10)
