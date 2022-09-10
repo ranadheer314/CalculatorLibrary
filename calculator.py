e541615717a4281ccdba96fd2bc5d6622a3f16d3
ranadheer314 <ranadheer314@users.noreply.github.com>
2022-09-10 15:25:59 -0400

    add unit tests for calculator

--- calculator.py
+++ calculator.py
@@ -3,8 +3,9 @@ Calculatorr Library containing basic math operations.
 """
 
 
-def add(first_term,second_term):
+def add(first_term, second_term):
     return first_term + second_term
 
-def subtract(first_term,second_term):
+
+def subtract(first_term, second_term):
     return first_term - second_term
---
+++ test_calculator.py
@@ -0,0 +1,11 @@
+import calculator
+
+
+class TestCalculator:
+
+    def test_addition(self):
+        assert 4 == calculator.add(2, 2)
+
+    
+    def test_subtraction(Self):
+        assert 2 == calculator.subtract(4, 2)
