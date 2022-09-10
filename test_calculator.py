c8068c683fdb0712163715d092b53b36876f6f7d
ranadheer314 <ranadheer314@users.noreply.github.com>
2022-09-10 16:30:44 -0400

    code style

--- test_calculator.py
+++ test_calculator.py
@@ -10,4 +10,4 @@ class TestCalculator:
         assert 2 == calculator.subtract(4, 2)
 
     def test_multipication(self):
-        assert 100 == calculator.multiply(10,10)
+        assert 100 == calculator.multiply(10, 10)
